13984a5811da5cb5ac1685f86d104d1ceb73677f
w13-tinfo431 <35407131+w13-tinfo431@users.noreply.github.com>
2018-02-24 14:23:43 -0800

    Initial commit

---
+++ ContosoUniversity/DAL/BloggingContext.cs
@@ -0,0 +1,14 @@
+﻿using ContosoUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace ContosoUniversity.DAL
+{
+    public class BloggingContext : DbContext
+    {
+        public BloggingContext()
+        {
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
